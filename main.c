37e7bb92ac8d4abb13178e1f1abb50fbadec9776
Timpd2 <timhome4@gmail.com>
2025-12-02 16:02:16 +0800

    add test

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
+int add(int a, int b) {
+    return a + b;
+}       
+int add_float(float a, float b) {
+    return a + b;
+}       
+
 int main() {
-    printf("Hello, World!\n");
+    int a=5;
+    int b=13;
+    int a=5.5;
+    int b=13.2;
+    printf("%d + %d =%d\n",a,b,add(a,b));
+    printf("%f + %f =%f\n",a,b,add_float(a,b));
     return 0;
 }
