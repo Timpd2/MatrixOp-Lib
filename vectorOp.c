134fd977100a54d7b46d12b5a1ce2c5e2e51c996
Chen <you@example.com>
2025-12-02 16:22:15 +0800

    Basic

---
+++ vectorOp.c
@@ -0,0 +1,29 @@
+#include<stdlib.h>
+#include"vectorOp.h"
+int* vAdd(int *A, int *B,int size)
+{
+    int *C=malloc(sizeof(int)*size);
+    int i = 0;
+    for(i = 0;i < size;i++){
+        C[i]=A[i]+B[i];
+    }
+    return C;
+}
+int* vSub(int *A, int *B,int size)
+{
+    int *C=malloc(sizeof(int)*size);
+    int i = 0;
+    for(i = 0;i < size;i++){
+        C[i]=A[i]-B[i];
+    }
+    return C;
+}
+int* vEW_Mul(int *A, int *B,int size) 
+{
+    int *C=malloc(sizeof(int)*size);
+    int i = 0;
+    for(i = 0;i < size;i++){
+        C[i]=A[i] * B[i];
+    }
+    return C;
+}
---
+++ vectorOp.h
@@ -0,0 +1,8 @@
+#ifndef VECTOROP_BASIC
+#define VECTOROP_BASIC
+
+int* vAdd(int *A,int *B,int size); 
+int* vSub(int *A,int *B,int size); 
+int vEW_Mul(int *A,int *B,int size); 
+
+#endif
